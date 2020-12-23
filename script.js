a39d2182d69c245489c9aa7d2816781caa33936f
Bjorn Yourey <byourey7783@gmail.com>
2020-12-23 18:56:33 -0500

    created 4 different functions and variables

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var upperCase = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "U", "V" ,"W", "X", "Y", "Z"]
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
-var numbers = 
-var specialCharacters = 
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"]
 
 
 
@@ -9,7 +9,15 @@ var specialCharacters =
 function selection() {    
     var numberLength = prompt("How long would you want your password to be?");
 // Will have to check if numberLength is a number
-if (numberLength <=8 && numberLength >=128) {
+var numbers = ("8 to 128");
+if (numbers == false) {
+    alert("Please enter a number between 8 to 128");
+} else {
+    alert("Your good to go");
+}
+
+
+if (numberLength >=8 && numberLength <=128) {
 alert("Please choose a password between 8 and 128 characters long");
 return;
 }
@@ -28,8 +36,35 @@ return;
 
 }
 
-// Need a function to get a random element from an array for uppercase to specialChar
+// Need a function to get a random element from an array for upperCase to specialCharacters
+function generaterandomupperCase() {
+    var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "U", "V" ,"W", "X", "Y", "Z"]
+return upperCase[Math.floor(Math.random() * upperCase.length)]
+}
+console.log(generaterandomupperCase())
+
+function generaterandomlowerCase() {
+    var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
+return lowerCase[Math.floor(Math.random() * lowerCase.length)]
+}
+console.log(generaterandomlowerCase())
+
+function generaterandomnumbers() {
+    var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    return numbers[Math.floor(Math.random() * numbers.length)]
+}
+console.log(generaterandomnumbers())
+
+function generaterandomspecialCharacters() {
+    var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"]
+    return specialCharacters[Math.floor(Math.random() * specialCharacters.length)]
+}
+console.log(generaterandomspecialCharacters())
+
+
+
 // Need a function to put the password together 
+
 
 
 

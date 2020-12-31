eccce8b9e1105a0341774c0f5ec48bb20b0f585e
Bjorn Yourey <byourey7783@gmail.com>
2020-12-31 15:38:18 -0500

    created if statements

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Created an Array for the 4 boxes
+
 var upperCase = ["A","B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "U", "V" ,"W", "X", "Y", "Z"]
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"]
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -9,13 +11,9 @@ var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"]
     var upperCasebox = document.getElementById("Uppercase");
     var lowerCasebox = document.getElementById("Lowercase");
     var numberBox = document.getElementById("Numbers");
-    var specialCharbox = document.getElementById("Specialcharacters");
-    const lengthEL = document.getElementById('passwordlength');
-    const generateEL = document.getElementById('generate');
-
-
-
-
+    var specialCharbox = document.getElementById("SpecialCharacters");
+    var pwdLength = document.getElementById("passwordlength");
+    var generateEL = document.getElementById("generate");
      
 
 
@@ -44,49 +42,71 @@ function generaterandomspecialCharacters() {
 }
 // console.log(generaterandomspecialCharacters())
 
-generateEL.addEventListener("click", function () {
-var uppercasecheck = upperCasebox.checked;
-// console.log(uppercasecheck)
-var lowercasecheck = lowerCasebox.checked;
-// console.log(lowercasecheck)
-var numberscheck = numberBox.checked;
-// console.log(numberscheck)
-var specialcharacterscheck = specialCharbox.checked;
-// console.log(specialcharacterscheck)
-// var pwlength = +lengthEL.value;
-if (!uppercasecheck && !lowercasecheck && !numberscheck && !specialcharacterscheck) {
+
+generateEL.addEventListener("click", function() {
+
+   if (upperCasebox.checked === false && lowerCasebox.checked === false && numberBox.checked === false && specialCharbox.checked === false) {
     alert("Tick 1 of the boxes below");
-    return;
-}
+   } else {
+    generatePassword()   
+  }
 
      // Need an object to put the user input in 
 
-    const randomFunc = {
-        lengthEL: lengthEL, 
-        upperCasebox: upperCasebox,  
-        lowerCasebox: lowerCasebox,
-        numberBox: numberBox,
-        specialCharbox: specialCharbox,    
-          
-     };
-     return randomFunc;
-
+    // 
 
 });
 
 function generatePassword() {
-    var gen = [];
-    gen.push(generaterandomupperCase()) 
-    gen.push(generaterandomlowerCase())
-    gen.push(generaterandomnumbers())
-    gen.push(generaterandomspecialCharacters())
-    console.log(gen);
-    // concantentate these 4 functions:
-    return gen;
+    let password = "";
+    var allcombo = "";
+var upperCaseCheck = upperCasebox.checked;
+// console.log(uppercasecheck)
+var lowerCaseCheck = lowerCasebox.checked;
+// console.log(lowercasecheck)
+var numbersCheck = numberBox.checked;
+// console.log(numberscheck)
+var specialCharactersCheck = specialCharbox.checked;
+// console.log(specialcharacterscheck)
+var passLengthval = pwdLength.value;
+// console.log(upperCaseCheck, lowerCaseCheck, numbersCheck, specialCharactersCheck, passLengthval);
 
+if(upperCaseCheck === true) {
+    password += generaterandomupperCase();
+}
+if(lowerCaseCheck === true) {
+    password += generaterandomlowerCase();
+}
+if(numbersCheck === true) {    
+    password += generaterandomnumbers();
+}
+if(specialCharactersCheck === true) {
+    password += generaterandomspecialCharacters();
 }
 
 
+for (i = 0; i<pwdLength; i++) {
+    password += allcombo(Math.floor(Math.random() * allcombo.length));
+}
+
+console.log(password);
+}
+
+
+
+// const passwordFunction = {
+//         pwdLength: passLengthval, 
+//         upperCasebox: upperCaseCheck,  
+//         lowerCasebox: lowerCaseCheck,
+//         numberBox: numbersCheck,
+//         specialCharbox: specialCharactersCheck,    
+//         };
+//      console.log(passwordFunction);
+    // var gen = [];
+    // gen.push(generaterandomupperCase(), generaterandomlowerCase(), generaterandomnumbers(), generaterandomspecialCharacters())
+    // // console.log(gen);
+    // // concantentate these 4 functions:
+    // return gen;
 
 
 
@@ -97,9 +117,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  
-
 
 
   passwordText.value = password;
